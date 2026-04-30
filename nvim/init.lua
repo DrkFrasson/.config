9c22362a66895e5e4ba29210658a09817058b488
Derek Frasson <derek-git@proton.me>
2026-04-30 15:22:12 -0300

    Added completion for "" and diferent types of {}

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,14 +13,17 @@ vim.opt.scrolloff = 10
 vim.keymap.set('n', '<leader>t', ':split | terminal\ni')
 vim.keymap.set('t', '<leader>t', 'exit\n')
 
-vim.keymap.set('i', '{', '{\n}<up>\n')
+vim.keymap.set('n', '{', 'i{\n}<up>\n')
+
+vim.keymap.set('i', '"', '""<Esc>i')
+vim.keymap.set('i', '{', '{}<Esc>i')
 vim.keymap.set('i', '(', '()<Esc>i')
 vim.keymap.set('i', '[', '[]<Esc>i')
 
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣',}
 
-vim.opt.spell = true
+vim.opt.spell = false
 vim.keymap.set('n', '<leader>st', ':set spell!\n')
 
 vim.keymap.set('n', '<leader>w', ':w\n')
