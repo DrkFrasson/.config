07981318bea785bbf77eeb859b4ff94c8bd1dfbf
Derek Frasson <derek.git@proton.me>
2026-05-09 00:50:16 -0300

    Fixed file type specific keybinds.

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,7 +9,7 @@ vim.opt.cursorline = true
 
 vim.opt.scrolloff = 10 
 
-vim.keymap.set('n', '<leader>t', ':split | terminal<NL>i')
+vim.keymap.set('n', '<leader>t', ':split | terminal<NL>i') -- [t]erminal
 vim.keymap.set('t', '<leader>t', 'exit<NL>')
 
 vim.keymap.set('n', '<leader>{{', 'i{<NL>}<up><NL>')
@@ -20,12 +20,14 @@ vim.keymap.set('i', '(', '()<Esc>i')
 vim.keymap.set('i', '[', '[]<Esc>i')
 vim.keymap.set('i', '<', '<><Esc>i')
 
---vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
---	patern = '*.rs',
---	callback = function()
---		vim.keymap.set('i', 'Vec', 'Vec<><Esc>i')
---	end,
---})
+vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
+	pattern = '*.rs',
+	callback = function()
+		vim.keymap.set('i', 'Vec', 'Vec<><Esc>i')
+		vim.keymap.set('n', 'fn', 'ifn ()<NL>{<NL>}<up><NL><up><up><left> <left>')
+		vim.keymap.set('n', 'pfn', 'ifn () -> :<NL>{<NL>}<up><NL><up><up><left> <left>')
+	end,
+})
 
 vim.keymap.set('n', '<leader>mc', 'i/*<NL>/<up><NL>') -- [M]ulti-line [C]omment
 vim.keymap.set('n', '<leader>oc', 'i// ') -- [O]ne-line [C]omment
@@ -34,8 +36,12 @@ vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣',}
 
 vim.opt.spell = false
-vim.keymap.set('n', '<leader>st', ':set spell!<NL>')
+vim.keymap.set('n', '<leader>st', ':set spell!<NL>') -- [s]pell [t]oggle
 
 vim.keymap.set('n', '<leader>w', ':w<NL>')
+vim.keymap.set('n', '<leader><shift>w', ':w!<NL>')
+
 vim.keymap.set('n', '<leader>q', ':q<NL>')
-vim.keymap.set('n', '<leader>ft', ':Ex<NL>')
+vim.keymap.set('n', '<leader><shift>q', ':q!<NL>')
+
+vim.keymap.set('n', '<leader>ft', ':Ex<NL>') -- [f]ile [t]ree
